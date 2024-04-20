fbb25e8b4e080174a8efd16d14e14b7fd282c3ba
kinza <kinzali96@gmail.com>
2024-04-20 14:14:56 +0300

    feat: adds the project links and images

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import devopsImage from '../assets/images/Devops.png';
-
+import EidSalesImage from '../assets/images/Eid-sales.jpeg';
+import PowerBIImage from '../assets/images/Power-bi.jpeg';
+import EventDataImage from '../assets/images/Event-data.jpeg';
 const Projects = () => {
   return (
     <section>
@@ -11,102 +12,89 @@ const Projects = () => {
       <div className="container mx-auto px-1">
         <div className="md:flex gap-10">
           <div className="text-center shadow-lg p-10 rounded-xl my-20 dark:bg-white flex-1">
-            {/* Image takes full width of the div */}
-            <div className="w-full">
+            <div className="w-full" style={{ width: '400px', height: '250px' }}>
               <img
-                className="mx-auto"
-                src={devopsImage}
-                width={800}
-                height={900}
+                className="mx-auto h-full w-full object-cover"
+                src={EventDataImage}
                 alt="web"
               />
             </div>
-            <p className="py-4 text-gray-800">
-              Crafting sophisticated web solutions tailored to your
-              requirements, employing a foundation in core design principles.
+            <p className="py-4">
+              <span className="text-teal-800 tracking-wider">
+                Data Warehouse:{' '}
+              </span>
+              <span className="text-gray-800 tracking-wider">
+                Collecting diverse event-based data, centralizing storage for
+                CRM, marketing, and visualization tools.
+              </span>
             </p>
             {/* Buttons with equal width using flex-1 */}
             <div className="flex justify-between mt-4 gap-20">
               <a
-                href="https://attir.azurewebsites.net"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex-1 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-              >
-                Live Preview
-              </a>
-              <a
-                href="https://github.com/AttiR/EChatCode"
+                href="https://github.com/Kinzali/Event-data-tracking"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex-1 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
               >
-                Code
+                Details
               </a>
             </div>
           </div>
-          
+
           <div className="text-center shadow-lg p-10 rounded-xl my-20 dark:bg-white flex-1">
             {/* Image takes full width of the div */}
-            <div className="w-full">
+            <div className="w-full" style={{ width: '400px', height: '250px' }}>
               <img
-                className="mx-auto"
-                src={devopsImage}
-                width={800}
-                height={900}
+                className="mx-auto h-full w-full object-cover"
+                src={EidSalesImage}
                 alt="web"
               />
             </div>
-            <p className="py-4 text-gray-800">
-              Crafting sophisticated web solutions tailored to your
-              requirements, employing a foundation in core design principles.
+
+            <p className="py-4">
+              <span className="text-teal-800 tracking-wider">
+                Python Sales Analysis:{' '}
+              </span>
+              <span className="text-gray-800 tracking-wider">
+                Enhance Eid sales performance through customer service insights
+                and sales optimization.
+              </span>
             </p>
             {/* Buttons with equal width using flex-1 */}
             <div className="flex justify-between mt-4 gap-20">
               <a
-                href="https://attir.azurewebsites.net"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex-1 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-              >
-                Live Preview
-              </a>
-              <a
-                href="https://github.com/AttiR/EChatCode"
+                href="https://github.com/Kinzali/Eid-Sales-Analysis"
                 className="flex-1 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
               >
-                Code
+                Details
               </a>
             </div>
           </div>
           <div className="text-center shadow-lg p-10 rounded-xl my-20 dark:bg-white flex-1">
             {/* Image takes full width of the div */}
-            <div className="w-full">
+            <div className="w-full" style={{ width: '400px', height: '250px' }}>
               <img
-                className="mx-auto"
-                src={devopsImage}
-                width={800}
-                height={900}
+                className="mx-auto h-full w-full object-cover"
+                src={PowerBIImage}
                 alt="web"
               />
             </div>
-            <p className="py-4 text-gray-800">
-              Crafting sophisticated web solutions tailored to your
-              requirements, employing a foundation in core design principles.
+            <p className="py-4">
+              <span className="text-teal-800 tracking-wider">
+                Power BI project:{' '}
+              </span>
+              <span className="text-gray-800 tracking-wider">
+                Analyzing network insights, identifying top-performing sectors,
+                and improving user throughput.
+              </span>
             </p>
             {/* Buttons with equal width using flex-1 */}
             <div className="flex justify-between mt-4 gap-20">
               <a
-                href="#live-preview"
-                className="flex-1 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-              >
-                Live Preview
-              </a>
-              <a
-                href="#code"
+                href="https://github.com/Kinzali/Power-BI-Telecom-Analysis?tab=readme-ov-file"
                 className="flex-1 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded"
               >
-                Code
+                Details
               </a>
             </div>
           </div>
